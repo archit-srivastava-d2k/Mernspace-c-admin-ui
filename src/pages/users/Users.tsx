d8b7315e14c1cee0a36abc239b84c583b835f165
Archit-D2K <i-archit.rivastava@d2kindia.com>
2025-12-06 12:50:35 +0530

    add drawer

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,89 +1,112 @@
-import { Breadcrumb, Space, Table } from 'antd';
-import { RightOutlined } from '@ant-design/icons';
-import { Link, Navigate } from 'react-router-dom';
-import { useQuery } from '@tanstack/react-query';
-import { getUsers } from '../../http/api';
-import type { User } from '../../Types';
-import { useAuthStore } from '../../store';
-import UsersFilter from './UsersFilter';
-
-
+import { Breadcrumb, Button, Drawer, Space, Table } from "antd";
+import { RightOutlined, PlusOutlined } from "@ant-design/icons";
+import { Link, Navigate } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import { getUsers } from "../../http/api";
+import type { User } from "../../Types";
+import { useAuthStore } from "../../store";
+import UsersFilter from "./UsersFilter";
+import React from "react";
 
 const columns = [
-    {
-        title: 'ID',
-        dataIndex: 'id',
-        key: 'id',
+  {
+    title: "ID",
+    dataIndex: "id",
+    key: "id",
+  },
+  {
+    title: "Name",
+    dataIndex: "firstName",
+    key: "firstName",
+    render: (_text: string, record: User) => {
+      return (
+        <div>
+          {record.firstName} {record.lastName}
+        </div>
+      );
     },
-    {
-        title: 'Name',
-        dataIndex: 'firstName',
-        key: 'firstName',
-        render: (_text: string, record: User) => {
-            return (
-                <div>
-                    {record.firstName} {record.lastName}
-                </div>
-            );
-        },
-    },
-    {
-        title: 'Email',
-        dataIndex: 'email',
-        key: 'email',
-    },
-    {
-        title: 'Role',
-        dataIndex: 'role',
-        key: 'role',
-    },
+  },
+  {
+    title: "Email",
+    dataIndex: "email",
+    key: "email",
+  },
+  {
+    title: "Role",
+    dataIndex: "role",
+    key: "role",
+  },
 ];
-  
-
 
 const Users = () => {
+  const [drawerOpen, setDrawerOpen] = React.useState(false);
+  const {
+    data: users,
+    isLoading,
+    isError,
+    error,
+  } = useQuery({
+    queryKey: ["users"],
+    queryFn: () => {
+      return getUsers().then((res) => res.data);
+    },
+  });
 
-  
-    const {
-        data: users,
-        isLoading,
-        isError,
-        error,
-    } = useQuery({
-        queryKey: ['users'],
-        queryFn: () => {
-            return getUsers().then((res) => res.data);
-        },
-        select: (res) => (res.data)
-    });
-    const { user } = useAuthStore();
+  console.log(users)
 
-    if (user?.role !== 'admin') {
-        return <Navigate to="/" replace={true} />;
-    }
+  const { user } = useAuthStore();
 
-    return (
+  if (user?.role !== "admin") {
+    return <Navigate to="/" replace={true} />;
+  }
 
-      
-        <>
-            <Space direction="vertical" size="large" style={{ width: '100%' }}>
-                <Breadcrumb
-                    separator={<RightOutlined />}
-                    items={[{ title: <Link to="/">Dashboard</Link> }, { title: 'Users' }]}
-                />
-                {isLoading && <div>Loading...</div>}
-                {isError && <div>{error.message}</div>}
+  return (
+    <>
+      <Space direction="vertical" size="large" style={{ width: "100%" }}>
+        <Breadcrumb
+          separator={<RightOutlined />}
+          items={[{ title: <Link to="/">Dashboard</Link> }, { title: "Users" }]}
+        />
+        {isLoading && <div>Loading...</div>}
+        {isError && <div>{error.message}</div>}
 
-                  <UsersFilter
-                    onFilterChange={(filterName: string, filterValue: string) => {
-                        console.log(filterName, filterValue);
-                    }}
-                />
+        <UsersFilter
+          onFilterChange={(filterName: string, filterValue: string) => {
+            console.log(filterName, filterValue);
+          }}
+        >
+          <Button
+            type="primary"
+            icon={<PlusOutlined />}
+            onClick={() => setDrawerOpen(true)}
+          >
+            Add User
+          </Button>
+        </UsersFilter>
 
-                <Table columns={columns} dataSource={users}  rowKey={'id'} />
+        <Table columns={columns} dataSource={ users?.data ||[]} rowKey={"id"} />
+
+        <Drawer
+          title="Create user"
+          width={720}
+          destroyOnHidden={true}
+          open={drawerOpen}
+          onClose={() => {
+            setDrawerOpen(false);
+          }}
+          footer={
+            <Space style={{ float: "right" }}>
+              <Button onClick={() => setDrawerOpen(false)}>Cancel</Button>
+              <Button type="primary">Submit</Button>
             </Space>
-        </>
-    );
+          }
+        >
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+        </Drawer>
+      </Space>
+    </>
+  );
 };
 
 export default Users;
